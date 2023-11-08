18b929d7bf22443f0b1dad5c126fd9dc54c6d45b
AnasJeg <anasjegoual.2000@gmail.com>
2023-11-08 20:11:39 +0100

    fv1.1

---
+++ ejbModule/dao/IDaoLocal.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.util.List;
+
+import entities.Filiere;
+import entities.Role;
+import entities.Student;
+import entities.User;
+import jakarta.ejb.Local;
+
+@Local
+public interface IDaoLocal<T> {
+
+
+		T create(T o);
+
+		T update(T o);
+
+		boolean delete(T o);
+
+		T findById(int id);
+
+		List<T> findAll();
+		public List<Student> findAllByFiliere(Filiere filiere);
+		public void affect(Role r, User u);
+	}
